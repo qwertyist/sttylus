252f54ae2ad70d664d0ed3f6b30cdd1ddc3580bb
asdf qwertyist <asdf@qwertyist.se>
2023-07-12 02:02:41 +0200

    setup eslint and prettier

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,38 +1,35 @@
-import { builtinModules } from 'module'
-import { defineConfig, Plugin } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2';
-import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
-import resolve from 'vite-plugin-resolve'
-import ViteComponents from "vite-plugin-components";
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import ViteComponents from 'vite-plugin-components'
 import pkg from '../../package.json'
-import visualizer from "rollup-plugin-visualizer"
+import visualizer from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  mode: process.env.NODE_ENV,
-  root: __dirname,
-  plugins: [
-    visualizer(),
-    createVuePlugin(),
-    ViteComponents({ transformer: "vue2"}),
-  ],
-  resolve: {
-		  alias: {
-				  'vue': 'vue/dist/vue.common'
-		  }
-  },
-  base: './',
-  build: {
-    /*commonjsOptions: {
+    mode: process.env.NODE_ENV,
+    root: __dirname,
+    plugins: [
+        visualizer(),
+        createVuePlugin(),
+        ViteComponents({ transformer: 'vue2' }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.common',
+        },
+    },
+    base: './',
+    build: {
+        /*commonjsOptions: {
 			include: [/node_modules/]
 	},*/
-    sourcemap: true,
-    outDir: '../../dist/renderer',
-  },
-  server: {
-    port: pkg.env.PORT,
-  },
-  define: {
-    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-  }
+        sourcemap: true,
+        outDir: '../../dist/renderer',
+    },
+    server: {
+        port: pkg.env.PORT,
+    },
+    define: {
+        __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
 })
