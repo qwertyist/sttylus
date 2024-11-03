3de7a5e4a01acabb1229b03bacf88ddec6c4f469
Botvid Johansson <asdf@qwertyist.se>
2024-11-03 16:14:57 +0000

    tabula: log to TABULA_LOG_FILE or /tmp/tabula.log

--- packages/tabula/env.go
+++ packages/tabula/env.go
@@ -8,10 +8,15 @@ import (
 
 type envVariables struct {
 	taskAuthToken string
+  logFile string
 }
 
 func getEnvVariables() *envVariables {
 	godotenv.Load()
 	taskAuthToken := os.Getenv("TASK_AUTH_TOKEN")
-	return &envVariables{taskAuthToken}
+  logFile := os.Getenv("TABULA_LOG_FILE")
+  if logFile == "" {
+    logFile = "/tmp/tabula.log"
+  }
+	return &envVariables{taskAuthToken,logFile}
 }
--- packages/tabula/main.go
+++ packages/tabula/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "log"
 	"net/http"
+  "os"
 
 	"github.com/qwertyist/tabula/ws"
 )
@@ -15,6 +17,16 @@ func init() {
 }
 
 func main() {
+  logFile, err := os.OpenFile(config.logFile, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+  if err != nil {
+    log.Panic(err)
+  }
+  defer logFile.Close()
+
+  log.SetOutput(logFile)
+  log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+  log.Println("Logging to custom file", config.logFile)
 
 	a := App{pools: make(map[string]*ws.Pool)}
 	a.Initialize(config)
